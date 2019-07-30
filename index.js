c06a3e6d0858c322bb8331ebea688771fdf69dbc
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2019-07-31 02:37:42 +0530

    Add Logistic Regression module

---
+++ index.js
@@ -0,0 +1,45 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
+const mnist = require('mnist-data');
+const plot = require('node-remote-plot');
+
+const LogisticRegression = require('./logistic-regression');
+
+function loadData() {
+    const mnistData = mnist.training(0, 60000);
+
+    const features = mnistData.images.values.map(image => _.flatMap(image));
+    const encodedLabels = mnistData.labels.values.map(label => {
+        const row = new Array(10).fill(0);
+        row[label] = 1;
+        return row;
+    });
+
+    return { features, labels: encodedLabels }
+}
+
+const { features, labels } = loadData();
+
+const regression = new LogisticRegression(features, labels, {
+    learningRate: 1,
+    iterations: 40,
+    batchSize: 500
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(1, 10000);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is', accuracy*100, '%');
+
+plot({
+    x: regression.costHistory.reverse()
+});
